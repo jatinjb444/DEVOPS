3e174493b38e4982ef5eb85b7e6735165e125e6a
Jatin <jatinjb05@gmail.com>
2025-06-11 22:58:23 +0530

    mongo

--- SkillSet/server/server.js
+++ SkillSet/server/server.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 const app = express();
 
 app.disable('x-powered-by');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 const allowedOrigins = [
   'http://localhost:3000',
@@ -39,6 +39,8 @@ mongoose.connect(process.env.MONGO_URI, {
 })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
+  
+  
 
 app.use('/api/auth', authRoutes);
 app.use('/api/skills', skillRoutes);
